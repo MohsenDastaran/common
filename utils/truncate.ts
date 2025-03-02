a7d035b93588cbeab46335725532697636899f62
MohsenDastaran <mohsen.dastaran@gmail.com>
2025-03-02 15:56:54 +0330

    fix: handle empty string case in truncate function

--- utils/truncate.ts
+++ utils/truncate.ts
@@ -1,2 +1,5 @@
-export const truncate = (str: string, max: number): string =>
-	str.length > max ? str.substring(0, max - 1) + "…" : str;
+export const truncate = (str: string, max: number): string => {
+	if (str) {
+		return str.length > max ? str.substring(0, max - 1) + "…" : str;
+	} else return str;
+};
